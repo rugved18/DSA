e9adba01022e1f0cfbbd57da2b4feaf1a32cc7db
rugved18 <rugvedkulk2003@gmail.com>
2023-12-19 21:39:32 +0530

    19/12

--- binary_search/search_element_rotated_sorted_array1.cpp
+++ binary_search/search_element_rotated_sorted_array1.cpp
@@ -2,13 +2,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int search(vector<int>& arr, int n, int k) {
+bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
+    int n = arr.size(); // size of the array.
     int low = 0, high = n - 1;
     while (low <= high) {
         int mid = (low + high) / 2;
 
         //if mid points the target
-        if (arr[mid] == k) return mid;
+        if (arr[mid] == k) return true;
+
+        //Edge case:
+        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
 
         //if left part is sorted:
         if (arr[low] <= arr[mid]) {
@@ -32,18 +40,17 @@ int search(vector<int>& arr, int n, int k) {
             }
         }
     }
-    return -1;
+    return false;
 }
 
 int main()
 {
-    vector<int> arr = {7, 8, 9, 1, 2, 3, 4, 5, 6};
-    int n = 9, k = 1;
-    int ans = search(arr, n, k);
-    if (ans == -1)
+    vector<int> arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+    int k = 3;
+    bool ans = searchInARotatedSortedArrayII(arr, k);
+    if (!ans)
         cout << "Target is not present.\n";
     else
-        cout << "The index is: " << ans << "\n";
+        cout << "Target is present in the array.\n";
     return 0;
 }
-

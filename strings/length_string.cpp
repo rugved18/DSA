36ca6045e94ddfd059259c4cf4a77301d69ca07b
rugved18 <rugvedkulk2003@gmail.com>
2023-12-29 20:19:24 +0530

    29/12

---
+++ strings/length_string.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+int length(char name[]){
+int cnt=0;
+for(int i=0;name[i]!='\0';i++){
+    cnt++;
+}
+return cnt;
+}
+
+int main(){
+    char name[20];
+    cout<<"enter the name"<<endl;
+    cin>>name;
+
+    cout<<"your name is ";
+    cout<<name<<endl;
+
+    cout<<"length "<<length(name)<<endl;
+
+    return 0;
+}

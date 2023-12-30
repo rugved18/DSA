b591776bb3c3b46d85031c6787559f72ed8a8e21
rugved18 <rugvedkulk2003@gmail.com>
2023-12-30 12:45:33 +0530

    30/12

---
+++ strings/reverse_string.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+int reverse(char name[], int n){
+int s =0;
+int e= n-1;
+while (s<e)
+{
+    swap(name[s++],name[e--]);
+}
+}
+int length(char name[]){
+int cnt=0;
+for(int i=0;name[i]!='\0';i++){
+    cnt++;
+}
+return cnt;
+}
+
+int main(){
+    char name[20];
+    cout<<"enter the name"<<endl;
+    cin>>name;
+
+    cout<<"your name is ";
+    cout<<name<<endl;
+      int len=length(name);
+    cout<<"length "<<len<<endl;
+    reverse(name,len);
+    cout<<"reverse of a string is : "<<name<<endl;
+    return 0;
+}

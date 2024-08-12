b530ccc22f55c5b3e49a49c00d1dfdee0e00bf00
rugved18 <rugvedkulk2003@gmail.com>
2024-08-12 08:27:32 +0530

    12-08-2024

---
+++ strings/random Note.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int charCount[26] = {0};
+        
+        for(char c : magazine) {
+            charCount[c - 'a']++;
+        }
+        
+        for(char c : ransomNote) {
+            if(charCount[c - 'a'] == 0) {
+                return false;
+            }
+            charCount[c - 'a']--;
+        }
+        
+        return true;
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    string ransomNote = "aab";
+    string magazine = "baa";
+    
+    bool result = solution.canConstruct(ransomNote, magazine);
+    
+    cout << (result ? "true" : "false") << endl;
+    
+    return 0;
+}

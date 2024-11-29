d06b9decb10c10fba25395d7765a6c68f3357417
Rugved Dhananjaya Kulkarni <123377912+rugved18@users.noreply.github.com>
2024-11-29 15:23:29 +0530

    Create q15.cpp

---
+++ restart/q15.cpp
@@ -0,0 +1,48 @@
+//Find the element that appears more than N/2 times in an array (Majority Element).
+
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+
+    for (int i = 0; i < n; i++) {
+        //selected element is v[i]
+        int cnt = 0;
+        for (int j = 0; j < n; j++) {
+            // counting the frequency of v[i]
+            if (v[j] == v[i]) {
+                cnt++;
+            }
+        }
+
+        // check if frquency is greater than n/2:
+        if (cnt > (n / 2))
+            return v[i];
+    }
+
+    return -1;
+}
+
+//better solution
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+
+    //declaring a map:
+    map<int, int> mpp;
+
+    //storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+        mpp[v[i]]++;
+    }
+
+    //searching for the majority element:
+    for (auto it : mpp) {
+        if (it.second > (n / 2)) {
+            return it.first;
+        }
+    }
+
+    return -1;
+}

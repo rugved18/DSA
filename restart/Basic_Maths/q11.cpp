ad284244304866da27ba7f5b505abddbadfa3da3
Rugved Dhananjaya Kulkarni <123377912+rugved18@users.noreply.github.com>
2024-12-05 17:43:41 +0530

    Create q11.cpp

---
+++ restart/Basic_Maths/q11.cpp
@@ -0,0 +1,14 @@
+//  number is prefect square or not 
+void square(int n){
+     
+     int sroot = sqrt(n);
+
+     if(sroot* sroot == n){
+        cout<<"true";
+     }
+     else{
+
+     cout<<"false";
+     }
+     
+}
